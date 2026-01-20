1ca45f6663c0eaaf55ab15f870f8da6577e952cd
Fovir <fovir@disroot.org>
2026-01-20 20:52:35 +0800

    feat: add the api to get repo information

--- tools/updater/internal/api/repo.go
+++ tools/updater/internal/api/repo.go
@@ -1,6 +1,27 @@
 package api
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
 type Repo struct {
 	Description string `json:"description"`
 	Stars       int    `json:"stargazers_count"`
+}
+
+// GetRepo Get repo information including description, number of stars, etc., via GitHub API.
+func (c *Client) GetRepo(ctx context.Context, owner, repo string) (*Repo, error) {
+	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/repos/%s/%s", owner, repo))
+	if err != nil {
+		return nil, err
+	}
+
+	var r Repo
+	if err := c.do(req, &r); err != nil {
+		return nil, err
+	}
+
+	return &r, nil
 }
