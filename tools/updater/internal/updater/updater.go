4a8630748534e8296f741a9f1ccf308d3ba0b0a8
Fovir <fovir@disroot.org>
2026-01-20 21:52:51 +0800

    feat: add `updater()`

---
+++ tools/updater/internal/updater/updater.go
@@ -0,0 +1,34 @@
+package updater
+
+import (
+	"context"
+	"path/filepath"
+	"strings"
+
+	"github.com/universtar-org/universtar-updater/internal/api"
+	"github.com/universtar-org/universtar-updater/internal/io"
+)
+
+// Update a file/user.
+func Update(client *api.Client, ctx context.Context, path string) error {
+	base := filepath.Base(path)
+	owner := strings.TrimSuffix(base, filepath.Ext(base))
+
+	projects, err := io.ReadYaml(path)
+	if err != nil {
+		return err
+	}
+
+	for i := range projects {
+		repo, err := client.GetRepo(ctx, owner, projects[i].Repo)
+		if err != nil {
+			return err
+		}
+		projects[i].Description = repo.Description
+		projects[i].Stars = repo.Stars
+	}
+
+	io.WriteYaml(projects, path)
+
+	return nil
+}
